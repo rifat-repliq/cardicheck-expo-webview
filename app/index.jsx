d1d5d84fbf338c1ae11ac317e616a7fb9083e3db
rifat-repliq <rifat.shikder@repliq.dev>
2023-09-14 15:09:04 +0600

    Fixed component naming issue

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,5 @@
 import WebView from "react-native-webview";
 
-export default function index() {
+export default function EntryPoint() {
   return <WebView source={{ uri: "https://telehealth-staging.vercel.app" }} />;
 }
