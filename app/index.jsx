32ad9fa5060a67774debcd6a8fa68e1ae2787972
rifat-repliq <rifat.shikder@repliq.dev>
2023-09-14 16:10:28 +0600

    Fixed the issue of exiting app when clicking on device back button or using back gesture

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useRef } from "react";
+import { BackHandler, Platform } from "react-native";
 import WebView from "react-native-webview";
 
 export default function EntryPoint() {
-  return <WebView source={{ uri: "https://telehealth-staging.vercel.app" }} />;
+  const webViewRef = useRef(null);
+
+  const onAndroidBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true; // prevent default behavior (exit app)
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+      return () => {
+        BackHandler.removeEventListener(
+          "hardwareBackPress",
+          onAndroidBackPress
+        );
+      };
+    }
+  }, []);
+
+  return (
+    <WebView
+      source={{ uri: "https://telehealth-staging.vercel.app" }}
+      ref={webViewRef}
+    />
+  );
 }
