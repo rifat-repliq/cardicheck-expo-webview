a8a6be2c65dca7eb5c138d748b535da77a765b41
rifat-repliq <rifat.shikder@repliq.dev>
2023-09-14 14:26:47 +0600

    Fixed URI

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { WebView } from "react-native-webview";
 
 export default function App() {
-  return (
-    <WebView source={{ uri: "https://telehealth-staging.vercel.app/r" }} />
-  );
+  return <WebView source={{ uri: "https://telehealth-staging.vercel.app" }} />;
 }
