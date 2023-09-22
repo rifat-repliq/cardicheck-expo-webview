7ddad3aab2ec5d453e9d76237dd7ff7be6fd1764
rifat-repliq <rifat.shikder@repliq.dev>
2023-09-22 12:29:06 +0600

    Disable zooming

--- app/index.jsx
+++ app/index.jsx
@@ -25,10 +25,13 @@ export default function EntryPoint() {
     }
   }, []);
 
+  const DISABLE_ZOOMING_JS = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
+
   return (
     <WebView
-      source={{ uri: "https://telehealth-staging.vercel.app" }}
       ref={webViewRef}
+      source={{ uri: "https://telehealth-staging.vercel.app" }}
+      injectedJavaScript={DISABLE_ZOOMING_JS}
     />
   );
 }
