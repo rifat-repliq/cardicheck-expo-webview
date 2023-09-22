fd697256225f88a7e6c35c1f52bcd5e3303bf8ae
rifat-repliq <rifat.shikder@repliq.dev>
2023-09-22 15:08:05 +0600

    Refactor disable zooming injectable

--- constants/injectables.js
+++ constants/injectables.js
@@ -1,1 +1,5 @@
-export const DISABLE_ZOOMING = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
+export const DISABLE_ZOOMING = `
+const meta = document.createElement('meta'); 
+meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'); 
+meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); 
+`;
