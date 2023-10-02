f20b1e79f9458b40d75abdab6225f7344fb4d084
rifat-repliq <rifat.shikder@repliq.dev>
2023-10-02 13:13:26 +0600

    Fix spinner component

--- components/shared/Spinner.jsx
+++ components/shared/Spinner.jsx
@@ -1,7 +1,7 @@
 import { ActivityIndicator, View } from "react-native";
 
 export default Spinner = () => (
-  <View style={{ flex: 1 }}>
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <ActivityIndicator size="large" color="#115E59" />
   </View>
 );
